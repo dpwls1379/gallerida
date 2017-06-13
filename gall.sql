71adaeeaa4045b69ef2a47c24383975ac3dc5866
dpwls1379 <rndeo1379@naver.com>
2017-06-13 16:08:37 +0900

    6월13일 연동

---
+++ gall.sql
@@ -0,0 +1,89 @@
+create table Gmember(
+id 			varchar2(20) 	primary key,
+pass 		varchar2(30) 	not null,
+name 		varchar2(10) 	not null,
+addr 		varchar2(50) 	not null,
+tel 		varchar2(20)	not null,
+email 		varchar2(50) 	not null,
+gender 		char(1) 		not null,
+birthday 	date 			not null
+);
+alter table Gmember modify(tel varchar2(20));
+select * from GMEMBER;
+insert into GMEMBER values('master','master','마스터','서울특별시 강남구 역삼동',
+'01011112222','master@master.com','M',sysdate);
+update Gmember set tel='01023232323' where id='master';
+
+create table Gproduct(
+pro_num 	number 			primary key,
+pro_cate 	varchar2(30) 	not null,
+pro_name 	varchar2(30) 	not null,
+pro_price 	number 			not null,
+pro_sale 	number 			default 0,
+pro_info 	varchar2(4000) 	not null,
+pro_image 	varchar2(20) 	default 'default.jpg',
+pro_date 	date 			not null,
+pro_count 	number 			not null,
+pro_cell 	number 			default 0
+);
+
+create table Gboardo(
+bo_num 		number 			primary key,
+pro_num 	number 			references Gproduct(pro_num),
+id 			varchar2(20) 	references Gmember(id),
+bo_subject 	varchar2(30) 	not null,
+bo_content 	varchar2(4000) 	not null,
+bo_date 	date 			not null,
+bo_image 	varchar2(30) 	default 'default.jpg',
+bo_read 	number 			default 0,
+bo_star 	number 			default 0,
+bo_del 		char(1) 		default 'n'
+);
+
+create table Gboardx(
+bx_num 		number 			primary key,
+id 			varchar2(20) 	references Gmember(id),
+bx_subject 	varchar2(30) 	not null,
+bx_content 	varchar2(4000) 	not null,
+bx_categ 	varchar2(30) 	not null,
+bx_date 	date 			not null,
+bx_read 	number 			default 0,
+bx_del 		char(1) 		default 'n'
+);
+
+create table Greplyo (
+reo_num 		number 			primary key,
+reo_content 	varchar2(100) 	not null,
+reo_date 		date 			not null,
+reo_del 		char(1) 		default 'n',
+id 				varchar2(20) 	references Gmember(id),
+bo_num 			number 			references Gboardo(bo_num)
+);
+
+create table Greplyx (
+rex_num 		number 			primary key,
+rex_content 	varchar2(100) 	not null,
+rex_date 		date 			not null,
+rex_del 		char(1) 		default 'n',
+id 				varchar2(20) 	references Gmember(id),
+bx_num 			number 			references Gboardx(bx_num)
+);
+
+create table Gcart (
+ct_num 		number 			primary key,
+ct_image 	varchar2(30) 	default 'default.jpg',
+ct_count	number 			default 0,
+ct_del 		char(1) 		default 'n',
+pro_num 	number 			references Gproduct(pro_num)
+);
+
+create table Gbuy (
+buy_num 	number 			primary key,
+buy_addr 	varchar2(50) 	not null,
+buy_memo 	varchar2(100),
+buy_date 	date,
+pro_num 	number 			references Gproduct(pro_num),
+ct_num		number 			references Gcart(ct_num),
+id 			varchar2(20) 	references Gmember(id)
+);
+
---
+++ src/main/java/gallerida/model/Member.java
@@ -0,0 +1,60 @@
+package gallerida.model;
+
+import java.sql.Date;
+
+public class Member {
+	
+	private String id;
+	private String pass;
+	private String addr;
+	private String tel;
+	private String email;
+	private String gender;
+	private Date birthday;
+	
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	
+	
+}
